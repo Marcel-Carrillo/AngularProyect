cfa2a0a18891941f81d0fd41d522cf267f7042db
marcel <mcarhue.ti@gmail.com>
2023-07-19 14:01:39 +0200

    añadido metodo en el service

--- src/app/services/restaurante.service.ts
+++ src/app/services/restaurante.service.ts
@@ -10,14 +10,14 @@ export class RestauranteService {
   static readonly URL_RESTAURANTES: string =
     'http://localhost:8081/restaurante';
 
-  static readonly URL_REST_PAGINAS: string = 
-  'http://localhost:8081/restaurante/paginas?page=0&size=2'
+  static readonly URL_REST_PAGINAS: string =
+    'http://localhost:8081/restaurante/paginas?page=0&size=2'
 
   cabeceras: HttpHeaders = new HttpHeaders({
     'Content-type': 'application/json',
   });
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
   getListaRestaurantes(): Observable<Array<Restaurante>> {
     return this.httpClient.get<Array<Restaurante>>(
@@ -41,8 +41,29 @@ export class RestauranteService {
     );
   }
 
-  deleteRestaurante(id:number) {
+  deleteRestaurante(id: number) {
     return this.httpClient.delete<Restaurante>(
       RestauranteService.URL_RESTAURANTES + "/" + id)
   }
+
+  postRestauranteConFoto(restaurante: Restaurante, archivo: File): Observable<Restaurante> {
+    //declaramos una variable local que represente el FormData
+    let formData = new FormData();
+
+    formData.append('nombre', restaurante.nombre);
+    formData.append('direccion', restaurante.direccion);
+    formData.append('barrio', restaurante.barrio);
+    formData.append('web', restaurante.web);
+    formData.append('fichaGoogle', restaurante.fichaGoogle);
+    formData.append('latitud', restaurante.latitud + '');
+    formData.append('longuitud', restaurante.longitud + '');
+    formData.append('precio', restaurante.precioMedio + '');
+    formData.append('especialidad1', restaurante.especialiadad1);
+    formData.append('especialidad2', restaurante.especialiadad2);
+    formData.append('especialidad3', restaurante.especialiadad3);
+    formData.append('archivo', archivo);
+
+    return this.httpClient.post<Restaurante>(RestauranteService.URL_RESTAURANTES + "/crear-con-foto", formData);
+  }
+
 }
